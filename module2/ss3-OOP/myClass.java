f9c71f9965a116a48cd9386ec698567221ae9d79
unknown <you@example.com>
2022-08-15 11:00:27 +0700

    add

---
+++ module2/ss3-OOP/myClass.java
@@ -0,0 +1,12 @@
+
+    public class myClass {
+        int x = 5;
+
+        public static void main(String[] args) {
+            myClass myObj1 = new myClass();  // Object 1
+            myClass myObj2 = new myClass();  // Object 2
+            System.out.println(myObj1.x);
+            System.out.println(myObj2.x);
+        }
+    }
+
